0fdb664de4b94103e4716c5e91ed25f7a3dde581
namae-no-nai <guilhermebluiz@hotmail.com>
2020-01-17 12:00:42 -0300

    image

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_165752) do
+ActiveRecord::Schema.define(version: 2020_01_16_215030) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
