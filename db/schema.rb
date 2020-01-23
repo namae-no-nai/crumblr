9746b136ac84be8c8b4b9ca273670b1b16d81862
namae-no-nai <guilhermebluiz@hotmail.com>
2020-01-23 11:19:35 -0300

    last submit, changes due

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_215030) do
+ActiveRecord::Schema.define(version: 2020_01_21_132808) do
+
+  create_table "hearts", force: :cascade do |t|
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
